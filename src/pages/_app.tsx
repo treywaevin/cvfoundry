93b882c4643febb8fb17b93e688f1480521aeddd
Aevin <aevin.eliares@gmail.com>
2024-09-10 21:18:27 -0700

    basic auth setup with clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { Open_Sans } from "next/font/google";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 
 const sans = Open_Sans({
   weight: ["800", "700"],
@@ -11,7 +14,9 @@ const sans = Open_Sans({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={sans.className}>
-      <Component {...pageProps} />
+      <ClerkProvider>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </main>
   )
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,11 +3,25 @@ import ResumeTopBar from '@/views/components/ResumePage/ResumeTopBar';
 import '../styles/globals.css';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme';
+import {
+  ClerkProvider,
+  SignIn,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
 export default function Resume() {
   return (
+
     <ThemeProvider theme={theme}>
-      <ResumeTopBar />
-      <Preview />
+      <SignedIn>
+        <ResumeTopBar />
+        <Preview />
+      </SignedIn>
+      <SignedOut>
+        <SignIn routing="hash"/>
+      </SignedOut>
     </ThemeProvider>
   );
 }
